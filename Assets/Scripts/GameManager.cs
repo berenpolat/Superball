ab3916739c8893bfec4dd1c87e1083ef0a0fde7f
berenelcn <polatberen24@gmail.com>
2024-02-02 21:57:23 +0300

    Start tuşuna basıldığında tamamen baştan başlıyor ve oyunu ilk başlattığında da artk boyle :8ball:

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,6 +18,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Text enScoreText;
     [SerializeField] private Text plScoreText;
     [SerializeField] private Text timerText;
+    [SerializeField] private Text countDownText;
 
     #endregion
 
@@ -37,27 +38,28 @@ public class GameManager : MonoBehaviour
     [SerializeField] private BallMovements bm;
 
     #endregion
-    
+
     #region Unity Methods
 
     private void Start()
     {
-        mainMenu.SetActive(false);
+        StopTheGame();
     }
 
     void Update()
     {
         timer += Time.deltaTime;
 
-        enScoreText.text = enemyScore.ToString();
-        plScoreText.text = playerScore.ToString();
-        timerText.text = Mathf.Floor(timer).ToString();
+            enScoreText.text = enemyScore.ToString();
+            plScoreText.text = playerScore.ToString();
+            timerText.text = Mathf.Floor(timer).ToString();
 
-        if (timer >= 60f)  
-        {
-            StopTheGame();
-        }
+            if (timer >= 60f)  
+            {
+                StopTheGame();
+            }
     }
+
     #endregion
     private void StopTheGame()
     {
@@ -73,6 +75,10 @@ public class GameManager : MonoBehaviour
     public void StartTheGame()
     {
         mainMenu.SetActive(false);
+        StartCountdown();
+    }
+    private void StartTheGameplay()
+    {
         enemyScore = 0;
         playerScore = 0;
         timer = 0;
@@ -82,6 +88,26 @@ public class GameManager : MonoBehaviour
         enemy.SetActive(true);
         player.SetActive(true);
         ball.SetActive(true);
+    }
+
+    private void StartCountdown()
+    {
+        StartCoroutine(CountdownCoroutine());
+    }
+
+    private System.Collections.IEnumerator CountdownCoroutine()
+    {
+
+        for (int i = 3; i > 0; i--)
+        {
+            countDownText.text = i.ToString();
+            yield return new WaitForSeconds(1f);
+        }
+
+        countDownText.text = "Go!";
+        yield return new WaitForSeconds(1f);
+        StartTheGameplay();
+        countDownText.text = "";
     }
    
 }

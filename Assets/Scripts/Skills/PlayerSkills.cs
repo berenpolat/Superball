0ae4e09a6953ef4bd49e30242cfb638bd64fb405
Enis Ay <enisay35@outlook.com>
2024-02-06 00:32:03 +0300

    c
    .

--- Assets/Scripts/Skills/PlayerSkills.cs
+++ Assets/Scripts/Skills/PlayerSkills.cs
@@ -11,6 +11,8 @@ public class PlayerSkills : MonoBehaviour
     public Vector2 offset;
     #endregion
 
+    #region Frezeer
+
     [SerializeField] GameObject player;
     [SerializeField] private GameObject frezeeBullet;
     [SerializeField] private Transform FirePoint;
@@ -19,38 +21,78 @@ public class PlayerSkills : MonoBehaviour
     public bool Frezeer = false;
     private bool frezeerUsed;
 
-   
+    #endregion
+
+    #region PowerShoot
+
+    [SerializeField] private Transform grabPoint;
+    [SerializeField] private Transform rayPoint;
+    [SerializeField] private float rayDistance;
+    [SerializeField] private float circleRadius = 1f;
+
+    private GameObject grabbedObject;
+    private int layerIndex;
+
+    #endregion
+
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        layerIndex = LayerMask.NameToLayer("ball");
         frezeerUsed = false;
+    }
+    void Update()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(rayPoint.position, transform.right, rayDistance);
+
+        if (Input.GetKey(KeyCode.K))
+        {
+            grabbedObject = hitInfo.collider.gameObject;
+            grabbedObject.GetComponent<Rigidbody2D>().isKinematic = true;
+            grabbedObject.transform.position = grabPoint.position;
+            grabbedObject.transform.SetParent(transform);
+
+        }
+        else if (Input.GetKeyUp(KeyCode.K))
+        {
+            grabbedObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            grabbedObject.transform.SetParent(null);
+            grabbedObject = null;
+        }
     }
 
     public void FrezeerButton()
     {
-       if(Frezeer == true)
-       { 
-          var Fbullet = Instantiate(frezeeBullet, FirePoint.position, FirePoint.rotation);
-          Fbullet.GetComponent<Rigidbody2D>().velocity = FirePoint.up * bulletSpeed;
+        if (Frezeer == true)
+        {
+            var Fbullet = Instantiate(frezeeBullet, FirePoint.position, FirePoint.rotation);
+            Fbullet.GetComponent<Rigidbody2D>().velocity = FirePoint.up * bulletSpeed;
             Frezeer = false;
             frezeerUsed = true;
-            if(frezeerUsed = true)
+            if (frezeerUsed = true)
             {
                 frezeerButton.interactable = false;
             }
             Destroy(FrezeerPowerUpInstance);
-       }
+        }
     }
     public void FrezeerActive()
     {
-        if(frezeerUsed == false)
+        if (frezeerUsed == false)
         {
-          Frezeer = true;
-          FrezeerPowerUpInstance = Instantiate(frezeerPowerUpEffect, (Vector2)player.transform.position + offset, Quaternion.identity);
-          FrezeerPowerUpInstance.transform.SetParent(player.transform);
-          frezeerButton.interactable = true;
+            Frezeer = true;
+            FrezeerPowerUpInstance = Instantiate(frezeerPowerUpEffect, (Vector2)player.transform.position + offset, Quaternion.identity);
+            FrezeerPowerUpInstance.transform.SetParent(player.transform);
+            frezeerButton.interactable = true;
         }
     }
-   
-  
+    void OnDrawGizmos()
+    {
+        // Gizmos ile CircleCast'in alanını çizelim.
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(rayPoint.position + (Vector3)transform.right * rayDistance, circleRadius);
+    }
+
+
+
 }

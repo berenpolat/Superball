5e7b98710950979f26bd0ce9af6e588e667df32b
Enis Ay <enisay35@outlook.com>
2024-02-13 14:21:55 +0300

    Donduran düşman bitti , düşman belirlenmiş zaman aralıklarında ateş edip bizi donduruyor , ateş etmeden önce effect çıkıyor. ekstra olarak skill controller ekleyebilirim.

--- Assets/Scripts/EnmeySkills.cs
+++ Assets/Scripts/EnmeySkills.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
@@ -12,18 +13,26 @@ public class EnmeySkills : MonoBehaviour
     [SerializeField] private Transform FrezeerSpwnPoint;
     [SerializeField] private float minDelay;
     [SerializeField] private float maxDelay;
-    [SerializeField] private float effectDuration;
     [SerializeField] private float bulletSpeed;
     private float nextActionTime = 0f;
     private GameObject currentEffect;
+    private GameObject Enemyy;
+    private GameObject FrezeerEffectInstance;
+    public static bool EnemyHaveFrezeer;
     #endregion
+
+    private void Start()
+    {
+        Enemyy = GameObject.FindGameObjectWithTag("Enemy");
+    }
+
     void Update()
     {
         if (Time.time > nextActionTime)
         {
             FireFrezeer();
             
-            SpawnEffect();
+           
 
             nextActionTime = Time.time + Random.Range(minDelay, maxDelay);
         }
@@ -33,20 +42,26 @@ public class EnmeySkills : MonoBehaviour
 
     void FireFrezeer()
     {
+        FrezeerEffectInstance = Instantiate(FrezeerEffect, transform.position, quaternion.identity);
+        FrezeerEffectInstance.transform.SetParent(Enemyy.transform);
+
+        StartCoroutine(Frezee());
+
+    }
+
+   
+
+    private IEnumerator Frezee()
+    {
+        yield return new WaitForSeconds(1f);
+        
         GameObject bullet = Instantiate(FrezeerPrefab, FrezeerSpwnPoint.position, quaternion.identity);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         
         Vector2 direction = (GameObject.FindGameObjectWithTag("Player").transform.position - FrezeerSpwnPoint.position).normalized;
         rb.velocity = direction * bulletSpeed;
         Destroy(bullet, 5f);
+        Destroy(FrezeerEffectInstance);
         
     }
-
-    void SpawnEffect()
-    {
-        GameObject effect = Instantiate(FrezeerEffect, transform.position, quaternion.identity);
-        currentEffect = effect;
-        Destroy(effect, effectDuration);
-    }
-    
 }
